95d59eda3255447d557cb2f06aa3d3ea436b4dae
xcheko51x <xcheko51x@gmail.com>
2019-09-02 19:58:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+// Importa la librería FPDF
 require 'FPDF/fpdf.php';
 
+
+// Creo datos estaticos para usarlos de ejemplo
+// En esta parte se podria obtener los datos de la BD
 $estudiante1 = [
 	"nombre" => "Juan Perez Perez",
 	"grado" => 5,
@@ -34,25 +38,40 @@ $estudiante3 = [
 
 $estudiantes = array($estudiante1, $estudiante2, $estudiante3);
 
+// Se crea un nuevo PDF
 $pdf = new fpdf();
 
+// Ciclo para recorrer todos los datos
 for($i = 0 ; $i < sizeof($estudiantes) ; $i++) {
-	$pdf->AddPage();
+	
+        // Añade una nueva pagina
+        $pdf->AddPage();
 
+        // Se selecciona la fuente, estilo y tamaño
 	$pdf->SetFont('Arial','B',16);
+        // Se inserta el nombre del alumno en el pdf
+        // EjeX, EjeY, texto a insertar
 	$pdf->Text(20, 30, "Alumno: ".$estudiantes[$i]["nombre"]);
 
+        // Se selecciona la fuente, estilo y tamaño
 	$pdf->SetFont('Arial', '',16);
+        // Se inserta el grado al pdf
+        // EjeX, EjeY, texto a insertar
 	$pdf->Text(20, 40, "Grado: ".$estudiantes[$i]["grado"]);
 
+        // Nos ubica en la posicion X, Y
 	$pdf->SetXY(10, 60);
 
+        // Rellena de color puesto R,G,B
 	$pdf->setFillColor(232, 232, 232);
 	$pdf->setFont('Arial', 'B', 12);
+        // Inserta la celda
+        // Ancho, alto, texto, borde, , ubicacion, color de fondo
 	$pdf->Cell(0, 10, 'MATERIAS', 1, 0, 'C', true);
 
 	$pdf->SetXY(10, 70);
 	$pdf->setFont('Arial', 'B', 12);
+        // Ancho, alto, texto, borde, , ubicacion, color de fondo
 	$pdf->Cell(30, 10, 'ESPANOL', 1, 0, 'C', true);
 	$pdf->Cell(40, 10, 'MATEMATICAS', 1, 0, 'C', true);
 	$pdf->Cell(30, 10, 'HISTORIA', 1, 0, 'C', true);
@@ -70,6 +89,7 @@ for($i = 0 ; $i < sizeof($estudiantes) ; $i++) {
 
 }
 
+// Cierra el PDF
 $pdf->Output();
 
 ?>
